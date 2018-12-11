ad9d16d82ac810a12b9e936b26790c81068a2ed1
cbjoy <45787572+cbjoy@users.noreply.github.com>
2018-12-11 17:05:29 +0600

    Add files via upload

---
+++ HangmanGame/src/hangmangame/HangFrame.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hangmangame;
+
+import java.awt.Image;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Chayti
+ */
+class HangFrame extends JFrame{
+    HangmanFrame frame=new HangmanFrame();
+    
+    
+}
